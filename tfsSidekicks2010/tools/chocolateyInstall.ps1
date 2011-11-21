2555355849dccc9cdfb4fcf91b1e03d7a795f3db
Brian Dukes <bdukes@engagesoftware.com>
2011-11-21 12:01:14 -0600

    Add TFS Sidekicks 2010 package

--- tfsSidekicks2010/tools/chocolateyInstall.ps1
+++ tfsSidekicks2010/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'tfsSidekicks2010' 'msi' '/quiet' 'http://www.attrice.info/downloads/TF_Sidekicks_3.1.1.msi' 
+﻿Install-ChocolateyPackage 'tfsSidekicks2010' 'msi' '/quiet' 'http://www.attrice.info/downloads/TF_Sidekicks_3.0.4.msi' 
