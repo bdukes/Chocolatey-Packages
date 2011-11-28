ea3944126e590c3d7bdc5fbb04d71f26a594f5fa
Brian Dukes <bdukes@engagesoftware.com>
2011-11-28 12:55:04 -0600

    Fix wrong silent argument in hg

--- hg/tools/chocolateyInstall.ps1
+++ hg/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿  Install-ChocolateyPackage 'hg' 'msi' '/silent' 'http://mercurial.selenic.com/release/windows/mercurial-2.0.0-x86.msi' 'http://mercurial.selenic.com/release/windows/mercurial-2.0.0-x64.msi' 
+﻿  Install-ChocolateyPackage 'hg' 'msi' '/quiet' 'http://mercurial.selenic.com/release/windows/mercurial-2.0.0-x86.msi' 'http://mercurial.selenic.com/release/windows/mercurial-2.0.0-x64.msi' 
