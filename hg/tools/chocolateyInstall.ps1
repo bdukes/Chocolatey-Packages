114d291fc807cd6469c240a7c8daed14e32f7094
Brian Dukes <bdukes@engagesoftware.com>
2011-11-28 12:47:01 -0600

    Add hg package

---
+++ hg/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿  Install-ChocolateyPackage 'hg' 'msi' '/silent' 'http://mercurial.selenic.com/release/windows/mercurial-2.0.0-x86.msi' 'http://mercurial.selenic.com/release/windows/mercurial-2.0.0-x64.msi' 
