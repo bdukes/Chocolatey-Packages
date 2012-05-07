0a7824ce6d4bc82ad4b7196f0d8677c3c6f87563
Brian Dukes <bdukes@engagesoftware.com>
2012-05-07 16:37:11 -0500

    Update hg to 2.2.1

--- hg/tools/chocolateyInstall.ps1
+++ hg/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿  Install-ChocolateyPackage 'hg' 'msi' '/quiet' 'http://mercurial.selenic.com/release/windows/mercurial-2.1.2-x86.msi' 'http://mercurial.selenic.com/release/windows/mercurial-2.1.2-x64.msi' 
+﻿  Install-ChocolateyPackage 'hg' 'msi' '/quiet' 'http://mercurial.selenic.com/release/windows/mercurial-2.2.1-x86.msi' 'http://mercurial.selenic.com/release/windows/mercurial-2.2.1-x64.msi' 
