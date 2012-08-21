b2cdd93a8c21f29affea650afe3466b076e17f7b
Brian Dukes <bdukes@engagesoftware.com>
2012-08-21 11:01:37 -0500

    Add Space Sniffer

---
+++ spacesniffer/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyZipPackage 'spacesniffer' 'http://www.uderzo.it/main_products/space_sniffer/files/spacesniffer_1_1_4_0.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
