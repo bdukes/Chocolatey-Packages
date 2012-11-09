cefe6eb07e500798e0216b2275e0c77e29d6bd75
Brian Dukes <bdukes@engagesoftware.com>
2012-11-09 14:21:49 -0600

    hg 2.4

--- hg/tools/chocolateyInstall.ps1
+++ hg/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿  Install-ChocolateyPackage 'hg' 'msi' '/quiet' 'http://mercurial.selenic.com/release/windows/mercurial-2.3.2-x86.msi' 'http://mercurial.selenic.com/release/windows/mercurial-2.3.2-x64.msi' 
+﻿  Install-ChocolateyPackage 'hg' 'msi' '/quiet' 'http://mercurial.selenic.com/release/windows/mercurial-2.4.0-x86.msi' 'http://mercurial.selenic.com/release/windows/mercurial-2.4.0-x64.msi' 
