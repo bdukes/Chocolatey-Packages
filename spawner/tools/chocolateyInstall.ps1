cf8ef4659d254e38a10e7b47b8bc8191f87e538c
Brian Dukes <bdukes@engagesoftware.com>
2013-01-28 14:34:09 -0600

    Fix bad URLs, spawner 0.1.8.1

--- spawner/tools/chocolateyInstall.ps1
+++ spawner/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'spawner' # arbitrary name for the package, used in messages
-$url = 'http://downloads.sourceforge.net/project/spawner/spawner/spawner-0.1.8/spawner-0.1.8-win32-x86_64.zip'
-$url64 = $url
+$url = 'http://downloads.sourceforge.net/project/spawner/spawner/spawner-0.1.8/spawner-0.1.8-win32-i386.zip'
+$url64 = 'http://downloads.sourceforge.net/project/spawner/spawner/spawner-0.1.8/spawner-0.1.8-win64-x86_64.zip'
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
