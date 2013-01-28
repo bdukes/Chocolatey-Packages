3710ee4faac24d87e1f47ef1de7ea553320a9b17
Brian Dukes <bdukes@engagesoftware.com>
2013-01-28 14:02:58 -0600

    spawner 0.1.8

--- spawner/tools/chocolateyInstall.ps1
+++ spawner/tools/chocolateyInstall.ps1
@@ -1,1 +1,5 @@
-﻿  Install-ChocolateyZipPackage 'spawner' 'http://downloads.sourceforge.net/project/spawner/spawner/spawner-0.1.7/spawner-0.1.7-win32-i386.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$packageName = 'spawner' # arbitrary name for the package, used in messages
+$url = 'http://downloads.sourceforge.net/project/spawner/spawner/spawner-0.1.8/spawner-0.1.8-win32-x86_64.zip'
+$url64 = $url
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
