a41def936ea3e8e1890b30c5fbaef39c7af99336
Brian Dukes <bdukes@engagesoftware.com>
2013-02-15 15:07:02 -0600

    Default Programs Editor

---
+++ DefaultProgramsEditor/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'DefaultProgramsEditor'
+$url = 'http://defaultprogramseditor.com/download'
+$url64 = $url
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
