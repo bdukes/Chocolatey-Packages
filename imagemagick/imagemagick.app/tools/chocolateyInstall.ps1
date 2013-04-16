86bba072c67d6254b911c4c702e63c4d059b8a1b
Brian Dukes <bdukes@engagesoftware.com>
2013-04-16 09:04:19 -0500

    imagemagick 6.8.4.10

--- imagemagick/imagemagick.app/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.app/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'imagemagick.app'
 $installerType = 'exe'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.4-9-Q16-x86-dll.exe'
-$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.4-9-Q16-x64-dll.exe'
+$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.4-10-Q16-x86-dll.exe'
+$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.4-10-Q16-x64-dll.exe'
 $silentArgs = '/SILENT /SP-'
 $validExitCodes = @(0)
 
