6faa66498ec4600660c6c2afc25f2bbbdab9cbc9
Brian Dukes <bdukes@engagesoftware.com>
2013-06-19 09:09:43 -0500

    svn 1.8.0

--- svn/tools/chocolateyInstall.ps1
+++ svn/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'svn' # arbitrary name for the package, used in messages
 $installerType = 'msi' #only one of these two: exe or msi
-$url = 'http://sourceforge.net/projects/win32svn/files/1.7.9/apache22/Setup-Subversion-1.7.9.msi' # download url
+$url = 'http://sourceforge.net/projects/win32svn/files/1.8.0/apache22/Setup-Subversion-1.8.0.msi' # download url
 $url64 = $url # 64bit URL here or just use the same as $url
 $silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
