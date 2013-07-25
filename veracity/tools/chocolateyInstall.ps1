68a11c23e4ddc44ba8d135ada7c4fcb03f1d094c
Brian Dukes <bdukes@engagesoftware.com>
2013-07-25 09:31:46 -0500

    veracity 2.5

--- veracity/tools/chocolateyInstall.ps1
+++ veracity/tools/chocolateyInstall.ps1
@@ -1,1 +1,7 @@
-﻿  Install-ChocolateyPackage 'veracity' 'msi' '/quiet' 'http://download.sourcegear.com/Veracity/release/2.1.0.10979/vv_x86__2.1.0.10979.msi' 'http://download.sourcegear.com/Veracity/release/2.1.0.10979/vv_x64__2.1.0.10979.msi' 
+﻿$packageName = 'veracity'
+$installerType = 'msi'
+$url = 'http://download.sourcegear.com/Veracity/release/2.5.0.11065/veracity_2.5.0.11065_x86.msi'
+$url64 = 'http://download.sourcegear.com/Veracity/release/2.5.0.11065/veracity_2.5.0.11065_x64.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
