ba74a0abf222e99fc5b8dbced4e2433bb8a22fbc
Brian Dukes <bdukes@engagesoftware.com>
2013-07-25 09:39:46 -0500

    imagemagick 6.8.6.7

--- imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'imagemagick.tool'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.6-6-Q16-x86-windows.zip'
+$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.6-7-Q16-x86-windows.zip'
 $url64 = $url
 
 try { 
