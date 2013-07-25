fce8569cb3e65f0c3001db837a68573277425e41
Brian Dukes <bdukes@engagesoftware.com>
2013-07-25 12:54:47 -0500

    get download link via regex, if direct installation fails
    imagemagick doesn't host old versions, so the link breaks every time a new version is published.

--- imagemagick/imagemagick.app/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.app/tools/chocolateyInstall.ps1
@@ -5,4 +5,42 @@ $url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.6-7-Q16-x
 $silentArgs = '/SILENT /SP-'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+try {
+    # Uninstall older version of imagemagick, otherwise the installation won’t be silent.
+    $regPath = 'HKLM:\SOFTWARE\ImageMagick\Current'
+    if (Test-Path $regPath) {
+        $uninstallPath = (Get-ItemProperty -Path $regPath).BinPath
+        $uninstallFilePath = "$uninstallPath\unins000.exe"
+        Uninstall-ChocolateyPackage $packageName $installerType $silentArgs $uninstallFilePath
+    }
+} catch {
+    Write-Warning "$packageName uninstallation failed, with message $($_.Exception.Message)"
+    Write-Warning "$packageName installation may not be silent"
+}
+
+try {
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+}
+catch {
+    try {
+        $tempDir = "$env:TEMP\chocolatey\$packageName"
+        if (-not (Test-Path $tempDir)) {New-Item $tempDir -ItemType directory}
+
+        $listUrl = 'http://www.imagemagick.org/download/binaries/'
+        $fileFullPath = "$tempDir\temp.html"
+
+        # Obtain the download URL of the latest version using regex
+        Get-ChocolateyWebFile 'HTML file containing the URL' $fileFullPath $listUrl
+        $content = Select-String -Path $fileFullPath -Pattern '-Q16-x86-dll.exe' | Select-Object -First 1
+        $content64 = Select-String -Path $fileFullPath -Pattern '-Q16-x64-dll.exe' | Select-Object -First 1
+        $url = $content -replace '.+(ImageMagick-)([\d-\.]+)(-Q16-x86-dll.exe).+', 'http://www.imagemagick.org/download/binaries/$1$2$3'
+        $url64 = $content64 -replace '.+(ImageMagick-)([\d-\.]+)(-Q16-x64-dll.exe).+', 'http://www.imagemagick.org/download/binaries/$1$2$3'
+
+        Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes
+
+        Write-ChocolateySuccess $packageName
+    } catch {
+        Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+        throw 
+    }
+}
