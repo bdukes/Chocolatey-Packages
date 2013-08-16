9fdaf1cfb796fc3832286e680dda1e701d3f3999
Gary Ewan Park <gep13@gep13.co.uk>
2013-08-16 20:50:24 +0100

    Added uninstall script for hg
    As per request on the hg disqus thread, a request was made for an uninstall script.  Using the same technique as used on other packages, added a call to WMI to uninstall the packge.

---
+++ hg/tools/chocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+$package = "Mercurial"
+$packageWildCard = $package + "*";
+
+try {
+	$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like $packageWildCard  -and ($_.Version -eq "2.7.0") }
+	$result = $app.Uninstall();
+	
+	Write-ChocolateySuccess $package
+}
+catch {
+	Write-ChocolateyFailure $package "$($_.Exception.Message)"
+	throw
+}
