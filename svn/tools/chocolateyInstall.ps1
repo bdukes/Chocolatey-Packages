dd7db833b5e057d67a353ccf4f7f68a4926535c5
Brian Dukes <bdukes@engagesoftware.com>
2013-08-19 13:37:28 -0500

    svn 1.8.1

--- svn/tools/chocolateyInstall.ps1
+++ svn/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'svn'
 $installerType = 'msi'
-$url = 'http://sourceforge.net/projects/win32svn/files/1.8.0/apache22/Setup-Subversion-1.8.0-1.msi'
+$url = 'http://sourceforge.net/projects/win32svn/files/1.8.1/Setup-Subversion-1.8.1-1.msi'
 $url64 = $url
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
