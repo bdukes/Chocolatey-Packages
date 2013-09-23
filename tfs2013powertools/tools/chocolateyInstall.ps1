2c5043e175aef2aecbae7a9af30f68663bfc651b
Brian Dukes <bdukes@engagesoftware.com>
2013-09-23 08:45:36 -0500

    Created TFS 2013 power tools package (prerelease)

---
+++ tfs2013powertools/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage 'tfs2013powertools' 'msi' '/quiet' 'http://visualstudiogallery.msdn.microsoft.com/f017b10c-02b4-4d6d-9845-58a06545627f/file/112253/2/Visual%20Studio%20Team%20Foundation%20Server%202013%20Power%20Tools.msi' 
