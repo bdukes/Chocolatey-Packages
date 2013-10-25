18ad6908b2da650cd0bd978e97c674730d7d46bd
Brian Dukes <bdukes@engagesoftware.com>
2013-10-25 14:42:01 -0500

    Add xunit and re-add conemu config

--- bdukes.boxstarter/tools/chocolateyInstall.ps1
+++ bdukes.boxstarter/tools/chocolateyInstall.ps1
@@ -1,4 +1,5 @@
-﻿Install-WindowsUpdate
+﻿Install-WindowsUpdate -AcceptEula
+Set-ExecutionPolicy RemoteSigned
 Set-ExplorerOptions -showHidenFilesFoldersDrives -showFileExtensions
 Enable-RemoteDesktop
 #Move-LibraryDirectory "Personal" "$env:UserProfile\skydrive\documents"
@@ -20,10 +21,10 @@ Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft SQL
 
 Install-ChocolateyFileAssociation ".dll" "$($Boxstarter.ChocolateyBin)\dotPeek.bat"
 
-#need to figure out why this is pointing to main boxstarter package instead of mine
-#copy-item (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'ConEmu.xml') -Force $env:ProgramFiles\ConEmu\ConEmu\ConEmu.xml
+copy-item (Join-Path (Get-PackageRoot($MyInvocation)) 'ConEmu.xml') -Force $env:ProgramFiles\ConEmu\ConEmu\ConEmu.xml
 
 #Install-ChocolateyVsixPackage JSLint http://visualstudiogallery.msdn.microsoft.com/1a417c37-4d6f-43ca-b753-6ea6eb5041fd/file/84638/4/JSLint.VS2012.vsix
+Install-ChocolateyVsixPackage xunit.runner.visualstudio http://visualstudiogallery.msdn.microsoft.com/463c5987-f82b-46c8-a97e-b1cde42b9099/file/66837/11/xunit.runner.visualstudio.vsix
 Install-ChocolateyVsixPackage WebEssentials2013 http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/17/WebEssentials2013.vsix
 #Install-ChocolateyVsixPackage WebWorkbench http://visualstudiogallery.msdn.microsoft.com/2b96d16a-c986-4501-8f97-8008f9db141a/file/53962/41/Mindscape.WebWorkbench.Integration.10.vsix
 
