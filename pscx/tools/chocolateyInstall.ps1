f0b5a2a0784760882f9dac944c0bb72b35ef69c1
Brian Dukes <bdukes@engagesoftware.com>
2013-12-05 11:43:25 -0600

    Update PSCX to version 3.1

--- pscx/tools/chocolateyInstall.ps1
+++ pscx/tools/chocolateyInstall.ps1
@@ -1,1 +1,8 @@
-Install-ChocolateyPackage 'pscx' 'msi' '/qb'  'https://pscx.codeplex.com/downloads/get/523236'
+$packageName = 'pscx'
+$installerType = 'msi'
+$url = 'https://pscx.codeplex.com/downloads/get/744915'
+$url64 = $url
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
