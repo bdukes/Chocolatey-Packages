34fbfb4a2cb8838a056bb3721679331aaaa4caba
Brian Dukes <bdukes@engagesoftware.com>
2014-05-07 08:49:55 -0500

    hg 3.0

--- hg/tools/chocolateyInstall.ps1
+++ hg/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'hg'
 $installerType = 'msi'
-$url = 'http://mercurial.selenic.com/release/windows/mercurial-2.9.2-x86.msi'
-$url64 = 'http://mercurial.selenic.com/release/windows/mercurial-2.9.2-x64.msi'
+$url = 'http://mercurial.selenic.com/release/windows/mercurial-3.0.0-x86.msi'
+$url64 = 'http://mercurial.selenic.com/release/windows/mercurial-3.0.0-x64.msi'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
