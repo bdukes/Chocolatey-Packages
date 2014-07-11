5ba776e92ab4120aa5a4b72d6f6a1da82a9d9787
Brian Dukes <bdukes@engagesoftware.com>
2014-07-11 08:09:24 -0500

    LightTable 0.6.7

--- LightTable/tools/chocolateyInstall.ps1
+++ LightTable/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 ﻿$packageName = 'LightTable'
-$url = 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.6/LightTableWin.zip'
+$url = 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.7/LightTableWin.zip'
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
