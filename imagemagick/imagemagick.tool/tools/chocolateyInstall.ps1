02553bd4f1a93d55732b196ba53bae29294e8893
Brian Dukes <bdukes@engagesoftware.com>
2014-08-20 13:20:39 -0500

    imagemagick 6.8.9.7

--- imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'imagemagick.tool'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.9-6-Q16-x86-windows.zip'
+$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.9-7-Q16-x86-windows.zip'
 
 try { 
   $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
