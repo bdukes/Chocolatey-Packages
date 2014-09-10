80e82e69f7015b36d0bd07337f5b3c8ce12e6b36
Brian Dukes <bdukes@engagesoftware.com>
2014-09-10 16:40:56 -0500

    add skitch package

---
+++ skitch/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'skitch'
+$installerType = 'exe'
+$url = 'http://evernote.com/download/get.php?file=SkitchWindows'
+$url64 = $url
+$silentArgs = '--mode unattended'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
