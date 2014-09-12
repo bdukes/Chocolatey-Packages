b725637a11002c6849a04e30009c30920cff120e
Brian Dukes <bdukes@engagesoftware.com>
2014-09-12 12:06:43 -0500

    Add beta packages for LINQPad (portable-only)

---
+++ linqpad/linqpad4.AnyCPU.portable - beta/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'linqpad4.AnyCPU.portable'
+$url = 'http://www.linqpad.net/GetFile.aspx?preview+LINQPad4-AnyCPU.zip'
+
+try {
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
