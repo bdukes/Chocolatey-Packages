025e011dd5ad542d84a6a087bb8244d1ac19c2a7
Brian Dukes <bdukes@engagesoftware.com>
2014-10-24 20:13:52 -0500

    Updates to PSCX package
     - Add 3.2.0 release notes
     - Don't use separate 64-bit URL (otherwise Chocolatey thinks there are
    different versions and has different messages).
     - Use rawgit CDN instead of GitHub directly for icon

--- pscx/tools/chocolateyInstall.ps1
+++ pscx/tools/chocolateyInstall.ps1
@@ -1,8 +1,7 @@
 ﻿$packageName = 'pscx'
 $installerType = 'msi'
 $url = 'https://pscx.codeplex.com/downloads/get/923562'
-$url64 = $url
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
