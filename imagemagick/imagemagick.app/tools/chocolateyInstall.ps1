1a0cf8217caa6dd128142baf3d6b8207e685846d
Brian Dukes <bdukes@engagesoftware.com>
2014-10-28 09:13:41 -0500

    imagemagick 6.8.8.9

--- imagemagick/imagemagick.app/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.app/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'imagemagick.app'
 $installerType = 'exe'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.9-8-Q16-x86-dll.exe'
-$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.9-8-Q16-x64-dll.exe'
+$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.9-9-Q16-x86-dll.exe'
+$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.9-9-Q16-x64-dll.exe'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
--- imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'imagemagick.tool'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.9-8-Q16-x86-windows.zip'
+$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.8.9-9-Q16-x86-windows.zip'
 
 try { 
   $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
