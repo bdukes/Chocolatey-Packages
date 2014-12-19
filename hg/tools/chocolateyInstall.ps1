700c62cd25ec8de8ece791d77ea126f20b8d9f19
Brian Dukes <bdukes@engagesoftware.com>
2014-12-19 12:42:03 -0600

    Add 3010 as valid exit code for hg

--- hg/tools/chocolateyInstall.ps1
+++ hg/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $installerType = 'msi'
 $url = 'http://mercurial.selenic.com/release/windows/mercurial-3.2.2-x86.msi'
 $url64 = 'http://mercurial.selenic.com/release/windows/mercurial-3.2.2-x64.msi'
 $silentArgs = '/quiet'
-$validExitCodes = @(0)
+$validExitCodes = @(0, 3010)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
 Install-ChocolateyPath "$env:SystemDrive\Program Files\Mercurial"
