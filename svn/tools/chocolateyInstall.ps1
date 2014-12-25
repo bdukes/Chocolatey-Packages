882ea59b5d5da098873d88c3833d7a539023ad58
Brian Dukes <bdukes@engagesoftware.com>
2014-12-25 15:58:47 -0600

    Don't use $url64 for svn

--- svn/tools/chocolateyInstall.ps1
+++ svn/tools/chocolateyInstall.ps1
@@ -1,9 +1,8 @@
 ﻿$packageName = 'svn'
 $installerType = 'msi'
 $url = 'http://sourceforge.net/projects/win32svn/files/1.8.10/Setup-Subversion-1.8.10.msi'
-$url64 = $url
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
 Install-ChocolateyPath "${env:ProgramFiles(x86)}\Subversion\bin"
