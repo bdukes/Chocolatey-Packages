2c16eec8c7ae485c347c79454303bb90fe18ddbf
Brian Dukes <bdukes@engagesoftware.com>
2014-12-25 15:56:31 -0600

    spawner 0.2.3

--- spawner/tools/chocolateyInstall.ps1
+++ spawner/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'spawner'
-$url = 'http://downloads.sourceforge.net/project/spawner/spawner/spawner-0.2.2/spawner-0.2.2-win32-i386.zip'
-$url64 = 'http://downloads.sourceforge.net/project/spawner/spawner/spawner-0.2.2/spawner-0.2.2-win64-x86_64.zip'
+$url = 'http://downloads.sourceforge.net/project/spawner/spawner/spawner-0.2.3/spawner-0.2.3-win32-i386.zip'
+$url64 = 'http://downloads.sourceforge.net/project/spawner/spawner/spawner-0.2.3/spawner-0.2.3-win64-x86_64.zip'
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
