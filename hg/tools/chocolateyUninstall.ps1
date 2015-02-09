93b594c472245b4f30bdb7d69aa3eea91f36637b
Brian Dukes <bdukes@engagesoftware.com>
2015-02-09 11:58:27 -0600

    Remove uninstall, should be handled by auto-uninstaller in choco

--- hg/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-try {
-    $regEntry = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ | ? { $_.GetValue('DisplayName') -like 'Mercurial*' }
-    if ($regEntry) {
-        $uninstallCommand = $regEntry.GetValue('QuietUninstallString')
-        & $uninstallCommand
-    }
-} catch {
-    throw $_
-}
