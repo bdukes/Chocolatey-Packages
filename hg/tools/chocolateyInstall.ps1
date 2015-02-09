57c7341e16e41ed695f3875f66278c0b76142604
Brian Dukes <bdukes@engagesoftware.com>
2015-02-09 09:30:40 -0600

    Update hg for 3.3 release
    Get install directory from correct place in registry
    Get uninstall command from registry (but not sure how to run it)

--- hg/tools/chocolateyInstall.ps1
+++ hg/tools/chocolateyInstall.ps1
@@ -1,11 +1,17 @@
-﻿$packageName = 'hg'
-$installerType = 'exe'
-$url = 'http://mercurial.selenic.com/release/windows/Mercurial-3.2.4.exe'
-$url64 = 'http://mercurial.selenic.com/release/windows/Mercurial-3.2.4-x64.exe'
-$silentArgs = '/VERYSILENT'
-$validExitCodes = @(0, 3010)
+﻿try {
+    $packageName = 'hg'
+    $installerType = 'exe'
+    $url = 'http://mercurial.selenic.com/release/windows/Mercurial-3.3.exe'
+    $url64 = 'http://mercurial.selenic.com/release/windows/Mercurial-3.3-x64.exe'
+    $silentArgs = '/VERYSILENT'
+    $validExitCodes = @(0, 3010)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
 
-$installDir = (Get-ItemProperty HKCU:\Software\Mercurial\InstallDir).'(default)'
-Install-ChocolateyPath $installDir
+    $regEntry = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ | ? { $_.GetValue('DisplayName') -like 'Mercurial*' }
+    if ($regEntry) {
+      Install-ChocolateyPath $regEntry.GetValue('InstallLocation')
+    }
+} catch {
+    throw $_
+}
--- hg/tools/chocolateyUninstall.ps1
+++ hg/tools/chocolateyUninstall.ps1
@@ -1,13 +1,9 @@
-$package = 'hg'
-$productName = 'Mercurial'
-
 try {
-	$app = Get-WmiObject -class Win32_Product -filter "Name LIKE '$productName%' AND Version='3.2.4'"
-	$result = $app.Uninstall();
-
-	Write-ChocolateySuccess $package
-}
-catch {
-	Write-ChocolateyFailure $package "$($_.Exception.Message)"
-	throw
+    $regEntry = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ | ? { $_.GetValue('DisplayName') -like 'Mercurial*' }
+    if ($regEntry) {
+        $uninstallCommand = $regEntry.GetValue('QuietUninstallString')
+        & $uninstallCommand
+    }
+} catch {
+    throw $_
 }
