9b9a77ddbbb84c7a5dc4e87eb85bb4ae7d170600
Brian Dukes <bdukes@engagesoftware.com>
2015-03-03 08:54:33 -0600

    hg 3.3.2

--- hg/tools/chocolateyInstall.ps1
+++ hg/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿try {
     $packageName = 'hg'
     $installerType = 'exe'
-    $url = 'http://mercurial.selenic.com/release/windows/Mercurial-3.3.exe'
-    $url64 = 'http://mercurial.selenic.com/release/windows/Mercurial-3.3-x64.exe'
+    $url = 'http://mercurial.selenic.com/release/windows/Mercurial-3.3.2.exe'
+    $url64 = 'http://mercurial.selenic.com/release/windows/Mercurial-3.3.2-x64.exe'
     $silentArgs = '/VERYSILENT'
     $validExitCodes = @(0, 3010)
 
-    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
 
     $regEntries = @(Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ | ? { $_.GetValue('DisplayName') -like 'Mercurial*' })
     foreach ($regEntry in $regEntries) {
