65af78ce358616571ef4dfddb5fcbd0d7d413e90
Brian Dukes <bdukes@engagesoftware.com>
2015-05-04 10:24:19 -0500

    hg 3.4

--- hg/tools/chocolateyInstall.ps1
+++ hg/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿try {
     $packageName = 'hg'
     $installerType = 'exe'
-    $url = 'http://mercurial.selenic.com/release/windows/Mercurial-3.3.3.exe'
-    $url64 = 'http://mercurial.selenic.com/release/windows/Mercurial-3.3.3-x64.exe'
+    $url = 'http://mercurial.selenic.com/release/windows/Mercurial-3.4.exe'
+    $url64 = 'http://mercurial.selenic.com/release/windows/Mercurial-3.4-x64.exe'
     $silentArgs = '/VERYSILENT'
     $validExitCodes = @(0, 3010)
 
