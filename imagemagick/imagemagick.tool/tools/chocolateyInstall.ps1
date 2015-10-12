9fa90ad46828374f5d1cb39e18434f9fd08765dd
Brian Dukes <bdukes@engagesoftware.com>
2015-10-12 09:59:17 -0500

    imagemagick 6.9.2-4

--- imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName = 'imagemagick.tool'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.9.2-3-portable-Q16-x86.zip'
-$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.9.2-3-portable-Q16-x64.zip'
+$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.9.2-4-portable-Q16-x86.zip'
+$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.9.2-4-portable-Q16-x64.zip'
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
 
 try { 
   Install-ChocolateyZipPackage $packageName $url $installDir $url64
 }
 catch {
-    $url = 'http://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/binaries/ImageMagick-6.9.2-3-portable-Q16-x86.zip'
-    $url64 = 'http://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/binaries/ImageMagick-6.9.2-3-portable-Q16-x64.zip'
+    $url = 'http://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/binaries/ImageMagick-6.9.2-4-portable-Q16-x86.zip'
+    $url64 = 'http://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/binaries/ImageMagick-6.9.2-4-portable-Q16-x64.zip'
     Install-ChocolateyZipPackage $packageName $url $installDir $url64
 }
