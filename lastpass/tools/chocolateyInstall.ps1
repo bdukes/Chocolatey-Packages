055985e8a23839e394eee2304ab7b403c2f2409e
Brian Dukes <bdukes@engagesoftware.com>
2016-01-14 11:25:48 -0600

    lastpass 4

--- lastpass/tools/chocolateyInstall.ps1
+++ lastpass/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'lastpass' 'exe' '-si --userinstallie --userinstallff --userinstallchrome --installforallusers' 'https://lastpass.com/download/cdn/lastpass.exe' 'https://lastpass.com/download/cdn/lastpass_x64.exe'
+﻿Install-ChocolateyPackage 'lastpass' 'exe' '-si --userinstallie --userinstallff --userinstallchrome' 'https://lastpass.com/download/cdn/lastpass.exe' 'https://lastpass.com/download/cdn/lastpass_x64.exe'
