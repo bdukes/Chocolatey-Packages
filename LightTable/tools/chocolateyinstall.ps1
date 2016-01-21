ee8bc9b73b276051c62b3fdf6a3d72b0afe2757e
Brian Dukes <bdukes@engagesoftware.com>
2016-01-21 13:14:08 -0600

    update lighttable to use non-alpha 0.8.0 package

--- LightTable/tools/chocolateyinstall.ps1
+++ LightTable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'lighttable'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/LightTable/LightTable/releases/download/0.8.0-alpha/lighttable-0.8.0-alpha-windows.zip'
+$url        = 'https://github.com/LightTable/LightTable/releases/download/0.8.0/lighttable-0.8.0-windows.zip'
 
 $packageArgs = @{
   packageName   = $packageName
--- LightTable/tools/chocolateyuninstall.ps1
+++ LightTable/tools/chocolateyuninstall.ps1
@@ -3,6 +3,6 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName = 'lighttable'
-$zipFileName = 'lighttable-0.8.0-alpha-windows.zip'
+$zipFileName = 'lighttable-0.8.0-windows.zip'
 
 UnInstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName $zipFileName
