cc1f48f63e6f3c2082888eb43e8f6b9e61e7d9fe
Brian Dukes <bdukes@engagesoftware.com>
2016-03-21 16:33:16 -0500

    Add elm-platform package
    This package was originally from
    https://github.com/chribben/ElmPlatformChocolateyPackage, I have taken it over.

---
+++ elm-platform/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'elm-platform'
+$url        = 'http://install.elm-lang.org/Elm-Platform-0.16.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+  softwareName  = 'Elm Platform*'
+  checksum      = 'dc10b901ab1305b37cd45b9b4d23362c43b6d060'
+  checksumType  = 'sha1'
+}
+
+Install-ChocolateyPackage @packageArgs
