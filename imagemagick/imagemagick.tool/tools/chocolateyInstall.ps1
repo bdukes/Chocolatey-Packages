a0673bfebe89aefeb50940dedd4db9f743421c87
Brian Dukes <bdukes@engagesoftware.com>
2016-05-20 08:02:41 -0500

    imagemagick 7.0.1-5

--- imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName = 'imagemagick.tool'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.1-0-portable-Q16-x86.zip'
-$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.1-0-portable-Q16-x64.zip'
+$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.1-5-portable-Q16-x86.zip'
+$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.1-5-portable-Q16-x64.zip'
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 try {
     Install-ChocolateyZipPackage $packageName $url $installDir $url64
 }
 catch {
-    $url = 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.1-0-portable-Q16-x86.zip'
-    $url64 = 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.1-0-portable-Q16-x64.zip'
+    $url = 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.1-5-portable-Q16-x86.zip'
+    $url64 = 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.1-5-portable-Q16-x64.zip'
     Install-ChocolateyZipPackage $packageName $url $installDir $url64
 }
