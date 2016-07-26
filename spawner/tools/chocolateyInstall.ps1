64fd36ba231e488328885c71848a3cf0407c9566
Brian Dukes <bdukes@engagesoftware.com>
2016-07-26 08:57:28 -0500

    Update spawner package
    Start from `choco new spawner`
    Adds checksums and metadata

--- spawner/tools/chocolateyInstall.ps1
+++ spawner/tools/chocolateyInstall.ps1
@@ -1,5 +1,20 @@
-﻿$packageName = 'spawner'
+﻿$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'spawner'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://downloads.sourceforge.net/project/spawner/spawner/spawner-0.2.4/spawner-0.2.4-win32-i386.zip'
 $url64 = 'https://downloads.sourceforge.net/project/spawner/spawner/spawner-0.2.4/spawner-0.2.4-win64-x86_64.zip'
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  url64bit      = $url64
+  checksum      = 'ab66b0c35d21ecf615a3270041b6151ee3b811be'
+  checksumType  = 'sha1'
+  checksum64    = '1d00b20ae69d0159a0f421ce99cce71189f50b03'
+  checksumType64= 'sha1'
+}
+
+Install-ChocolateyZipPackage @packageArgs
