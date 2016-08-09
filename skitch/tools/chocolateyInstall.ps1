0e93278e23792e987adff45b46c4eb13a1425304
Brian Dukes <dukes.brian@gmail.com>
2016-08-08 22:05:41 -0500

    Remove Skitch
    It is no longer distributed

--- skitch/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'skitch'
-$installerType = 'exe'
-$url = 'http://evernote.com/download/get.php?file=SkitchWindows'
-$silentArgs = '--mode unattended'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
