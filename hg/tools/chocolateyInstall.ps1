0d5946cd6c56b3f16a3b582f2561450bb4ef1e42
Brian Dukes <bdukes@engagesoftware.com>
2016-08-17 14:52:43 -0500

    hg 3.9

--- hg/tools/chocolateyInstall.ps1
+++ hg/tools/chocolateyInstall.ps1
@@ -1,13 +1,21 @@
-﻿try {
-    $packageName = 'hg'
-    $installerType = 'exe'
-    $url = 'https://www.mercurial-scm.org/release/windows/Mercurial-3.8.3.exe'
-    $url64 = 'https://www.mercurial-scm.org/release/windows/Mercurial-3.8.3-x64.exe'
-    $silentArgs = '/VERYSILENT'
-    $validExitCodes = @(0, 3010)
+﻿$ErrorActionPreference = 'Stop';
 
-    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = 'hg'
+  installerType = 'exe'
+  url           = 'https://www.mercurial-scm.org/release/windows/Mercurial-3.9.exe'
+  url64bit      = 'https://www.mercurial-scm.org/release/windows/Mercurial-3.9-x64.exe'
+  checksum      = 'FC91246C90D08698022FD577668B63C5D653DF1EA94F459C07809946EEA10C8B'
+  checksumType  = 'sha256'
+  checksum64    = 'B58CBCD3EFD9BC46D2B7B05BF5F7FA281F3CB51592DEB34F59AC46C178E7D05E'
+  checksumType64= 'sha256'
+  silentArgs    = '/VERYSILENT'
+  validExitCodes= @(0, 3010)
+}
 
+Install-ChocolateyPackage @packageArgs
+
+try {
     $regEntries = @(Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\ | ? { $_.GetValue('DisplayName') -like 'Mercurial*' })
     foreach ($regEntry in $regEntries) {
         $installLocation = $regEntry.GetValue('InstallLocation')
