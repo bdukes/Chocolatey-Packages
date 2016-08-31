0035c73c5f8dc85b34dbde6cdad78259d9f3606a
Brian Dukes <bdukes@engagesoftware.com>
2016-08-31 10:16:46 -0500

    Update Imagemagick

--- automatic/imagemagick.app/update.ps1
+++ automatic/imagemagick.app/update.ps1
@@ -5,10 +5,10 @@ $releases = 'https://www.imagemagick.org/script/binary-releases.php'
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^\s*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
      }
 }
@@ -21,8 +21,10 @@ function global:au_GetLatest {
 
     $versionMatch = $url[0] | select-string -Pattern $re
     $version = $versionMatch.Matches[0].Groups[1].Value -replace '-', '.'
+    $url64 = $url[0] -replace 'http://', 'https://'
+    $url32 = $url[1] -replace 'http://', 'https://'
 
-    $Latest = @{ URL32 = $url[1]; URL64 = $url[0]; Version = $version }
+    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
     return $Latest
 }
 
