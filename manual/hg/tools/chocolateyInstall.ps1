1fb99468c99f03f574237479a4df5aa5f7e87d0e
Brian Dukes <dukes.brian@gmail.com>
2016-09-19 20:50:28 -0500

    hg 3.9.1

--- manual/hg/tools/chocolateyInstall.ps1
+++ manual/hg/tools/chocolateyInstall.ps1
@@ -3,15 +3,16 @@
 $packageArgs = @{
   packageName   = 'hg'
   installerType = 'exe'
-  url           = 'https://www.mercurial-scm.org/release/windows/Mercurial-3.9.exe'
-  url64bit      = 'https://www.mercurial-scm.org/release/windows/Mercurial-3.9-x64.exe'
-  checksum      = 'FC91246C90D08698022FD577668B63C5D653DF1EA94F459C07809946EEA10C8B'
+  url           = 'https://www.mercurial-scm.org/release/windows/Mercurial-3.9.1.exe'
+  url64bit      = 'https://www.mercurial-scm.org/release/windows/Mercurial-3.9.1-x64.exe'
+  checksum      = '68284536732A13D2238BCB73EAC67C4248D5370F8FCA0DB7C4DA8159C5BA9BA0'
   checksumType  = 'sha256'
-  checksum64    = 'B58CBCD3EFD9BC46D2B7B05BF5F7FA281F3CB51592DEB34F59AC46C178E7D05E'
+  checksum64    = 'E7381588237965503644383ED8322A0072A6899D1C35D606A065B1CBE01B15D7'
   checksumType64= 'sha256'
   silentArgs    = '/VERYSILENT'
   validExitCodes= @(0, 3010)
 }
+
 
 Install-ChocolateyPackage @packageArgs
 
