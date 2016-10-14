3d5bce69c6a13cbb04cba4688c45a78bcf501eb2
Brian Dukes <bdukes@engagesoftware.com>
2016-10-14 14:37:47 -0500

    Use correct parameter name

--- automatic/imagemagick.tool/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -2,9 +2,9 @@
   packageName    = 'imagemagick.tool'
   url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.3-0-portable-Q16-x86.zip'
   url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.3-0-portable-Q16-x64.zip'
-  installDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksum       = '5F04DC4E47E612B19CD1322AF4D19A8A7DBF147D5BF598D3498C19F3E122DB2D'
   checksum64     = '4C266EB5786BC1B0AE604FFBD8993F648817B2B5BA2CFB1DD8DF335980FCBFFD'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
 }
