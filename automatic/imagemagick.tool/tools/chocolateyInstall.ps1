07a01b10e2a1a1be180eaeecdc19cc884a8d166f
Brian Dukes <bdukes@engagesoftware.com>
2016-10-14 14:13:20 -0500

    Move imagemagick.tool to automatic

---
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageArgs = @{
+  packageName    = 'imagemagick.tool'
+  url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.3-0-portable-Q16-x86.zip'
+  url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.3-0-portable-Q16-x64.zip'
+  installDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  checksum       = '5F04DC4E47E612B19CD1322AF4D19A8A7DBF147D5BF598D3498C19F3E122DB2D'
+  checksum64     = '4C266EB5786BC1B0AE604FFBD8993F648817B2B5BA2CFB1DD8DF335980FCBFFD'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+}
+
+try {
+    Get-WebHeaders $packageArgs.url
+}
+catch {
+    $packageArgs.url = $packageArgs.url -replace 'https://www.imagemagick.org/download/binaries/', 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/'
+    $packageArgs.url64 = $packageArgs.url64 -replace 'https://www.imagemagick.org/download/binaries/', 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/'
+}
+
+Install-ChocolateyZipPackage @packageArgs
