820e7afa9f1c2d1e6e33e91097d981b657d9e366
Brian Dukes <bdukes@engagesoftware.com>
2016-10-27 11:53:33 -0500

    imagemagick 6.9.6-2
    Update install script to better support fallback

--- imagemagick/imagemagick.app/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.app/tools/chocolateyInstall.ps1
@@ -1,14 +1,28 @@
-﻿$packageName = 'imagemagick.app'
-$installerType = 'exe'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.9.3-9-Q16-x86-dll.exe'
-$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.9.3-9-Q16-x64-dll.exe'
-$silentArgs = '/VERYSILENT'
-$validExitCodes = @(0)
+﻿$packageArgs = @{
+  packageName    = 'imagemagick.app'
+  installerType  = 'exe'
+  url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.6-2-Q16-x86-dll.exe'
+  url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.6-2-Q16-x64-dll.exe'
+  checksum       = '9A67B7629B32E3C88A24C92AC03B93ED52216EF713B7DD28DE89CF2A6C0CD272'
+  checksum64     = '0328D3AB90BC7355A8636D1AAFBCB80021871115E7E79506CA046239CAA2A137'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0)
+}
+
+try {
+    Get-WebHeaders $packageArgs.url
+}
+catch {
+    $packageArgs.url = $packageArgs.url -replace 'https://www.imagemagick.org/download/binaries/', 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/'
+    $packageArgs.url64 = $packageArgs.url64 -replace 'https://www.imagemagick.org/download/binaries/', 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/'
+}
 
 if ($env:chocolateyPackageParameters) {
     $packageParams = ConvertFrom-StringData $env:chocolateyPackageParameters.Replace(" ", "`n")
     if ($packageParams.InstallDevelopmentHeaders) {
-        $silentArgs = $silentArgs + ' /MERGETASKS=install_devel'
+        $packageArgs.silentArgs = $packageArgs.silentArgs + ' /MERGETASKS=install_devel'
     }
 }
 
@@ -21,19 +35,12 @@ try {
     if (Test-Path $regPath) {
         $uninstallPath = (Get-ItemProperty -Path $regPath).BinPath
         $uninstallFilePath = "$uninstallPath\unins000.exe"
-        Uninstall-ChocolateyPackage $packageName $installerType $silentArgs $uninstallFilePath
+        Uninstall-ChocolateyPackage $packageArgs.packageName $packageArgs.installerType $packageArgs.silentArgs $uninstallFilePath
     }
 } catch {
     Write-Warning "$packageName uninstallation failed, with message $($_.Exception.Message)"
     Write-Warning "$packageName installation may not be silent"
 }
 
-try {
-    Write-Verbose "Installing with arguments: $silentArgs"
-    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-}
-catch {
-    $url = 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-6.9.3-9-Q16-x86-dll.exe'
-    $url64 = 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-6.9.3-9-Q16-x64-dll.exe'
-    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-}
+Write-Verbose "Installing with arguments: $($packageArgs.silentArgs)"
+Install-ChocolateyPackage @packageArgs
--- imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
+++ imagemagick/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,13 +1,20 @@
-﻿$packageName = 'imagemagick.tool'
-$url = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.9.3-9-portable-Q16-x86.zip'
-$url64 = 'http://www.imagemagick.org/download/binaries/ImageMagick-6.9.3-9-portable-Q16-x64.zip'
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$packageArgs = @{
+  packageName    = 'imagemagick.tool'
+  url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.6-2-portable-Q16-x86.zip'
+  url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.6-2-portable-Q16-x64.zip'
+  checksum       = '6C0AE8E4EE665F6CCD7A4A02639AA76AA505B4EB89362FB5A53CB304287DAAC2'
+  checksum64     = 'C8F3C5FC9C693B88382BBBF87329D8ACF7559DC74A44921573D491A27207E39E'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+}
 
 try {
-    Install-ChocolateyZipPackage $packageName $url $installDir $url64
+    Get-WebHeaders $packageArgs.url
 }
 catch {
-    $url = 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-6.9.3-9-portable-Q16-x86.zip'
-    $url64 = 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-6.9.3-9-portable-Q16-x64.zip'
-    Install-ChocolateyZipPackage $packageName $url $installDir $url64
+    $packageArgs.url = $packageArgs.url -replace 'https://www.imagemagick.org/download/binaries/', 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/'
+    $packageArgs.url64 = $packageArgs.url64 -replace 'https://www.imagemagick.org/download/binaries/', 'http://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/'
 }
+
+Install-ChocolateyZipPackage @packageArgs
