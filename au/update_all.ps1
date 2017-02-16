e0d4e4122ac30819943af197e26f7281ad59cb28
Brian Dukes <dukes.brian@gmail.com>
2017-02-15 22:22:14 -0600

    Update AU pieces

--- au/update_all.ps1
+++ au/update_all.ps1
@@ -1,29 +1,39 @@
 # AU Packages Template: https://github.com/majkinetor/au-packages-template
 
-param($Name = $null)
+param([string[]] $Name, [string] $ForcedPackages, [string] $Root = $PSScriptRoot)
 
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
-    Timeout    = 100                                        #Connection timeout in seconds
-    Threads    = 10                                         #Number of background jobs to use
-    Push       = $Env:au_Push -eq 'true'                    #Push to chocolatey
-    PluginPath = ''                                         #Path to user plugins
+    Timeout       = 100                                     #Connection timeout in seconds
+    UpdateTimeout = 1200                                    #Update timeout in seconds
+    Threads       = 10                                      #Number of background jobs to use
+    Push          = $Env:au_Push -eq 'true'                 #Push to chocolatey
+    PluginPath    = ''                                      #Path to user plugins
 
     Report = @{
         Type = 'markdown'                                   #Report type: markdown or text
-        Path = "$PSScriptRoot\Update-AUPacakges.md"         #Path where to save the report
+        Path = "$PSScriptRoot\..\Update-AUPackages.md"      #Path where to save the report
         Params= @{                                          #Report parameters:
             Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
-            UserMessage = ''                                #  Markdown, Text: Custom user message to show
+            UserMessage = "[History](#update-history)"      #  Markdown, Text: Custom user message to show
+            NoIcons     = $false                            #  Markdown: don't show icon
+            IconSize    = 32                                #  Markdown: icon size
+            Title       = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
         }
     }
 
+    History = @{
+        Lines = 30                                          #Number of lines to show
+        Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
+        Path = "$PSScriptRoot\..\Update-History.md"         #Path where to save history
+    }
+
     Gist = @{
-        Id          = $Env:gist_id                          #Your gist id or leave empty for anonymous
-        ApiKey      = $Env:github_api_key                   #Your github api key
-        Path        = "$PSScriptRoot\Update-AUPacakges.md"  #List of files to add to gist
+        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
+        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+        Path   = "$PSScriptRoot\..\Update-AUPackages.md", "$PSScriptRoot\..\Update-History.md"       #List of files to add to the gist
     }
 
     Git = @{
@@ -33,25 +43,36 @@ $Options = [ordered]@{
 
     RunInfo = @{
         Exclude = 'password', 'apikey'                      #Option keys which contain those words will be removed
-        Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
+        Path    = "$PSScriptRoot\..\update_info.xml"           #Path where to save the run info
     }
 
     Mail = if ($Env:mail_user) {
             @{
-                To          = $Env:mail_user
-                Server      = $Env:mail_server
-                UserName    = $Env:mail_user
-                Password    = $Env:mail_pass
-                Port        = $Env:mail_port
-                EnableSsl   = $Env:enable_ssl -eq 'true'
-                Attachments = "$PSScriptRoot\update_info.xml"
+                To         = $Env:mail_user
+                Server     = $Env:mail_server
+                UserName   = $Env:mail_user
+                Password   = $Env:mail_pass
+                Port       = $Env:mail_port
+                EnableSsl  = $Env:mail_enablessl -eq 'true'
+                Attachment = "$PSScriptRoot\..\update_info.xml"
                 UserMessage = ''
                 SendAlways  = $false                        #Send notifications every time
              }
            } else {}
+
+    ForcedPackages = $ForcedPackages -split ' '
+    BeforeEach = {
+        param($PackageName, $Options )
+        $p = $Options.ForcedPackages | ? { $_ -match "^${PackageName}(?:\:(.+))*$" }
+        if (!$p) { return }
+
+        $global:au_Force   = $true
+        $global:au_Version = ($p -split ':')[1]
+    }
 }
 
-$global:au_Root = "$PSScriptRoot\..\automatic"                           #Path to the AU packages
-$info = updateall -Name $Name -Options $Options
+if ($ForcedPackages) { Write-Host "FORCED PACKAGES: $ForcedPackages" }
+$global:au_Root = "$Root\..\automatic"                                    #Path to the AU packages
+$global:info = updateall -Name $Name -Options $Options
 
-if ($info.error_count.total) { throw 'Errors during update' }
+#if ($global:info.error_count.total) { throw 'Errors during update' }
