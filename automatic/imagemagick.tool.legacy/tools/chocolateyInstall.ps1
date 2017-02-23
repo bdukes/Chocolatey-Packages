721b23f43469b3d3b6fb8a53d53cef047af6db96
Brian Dukes (via AppVeyor) <bdukes@engagesoftware.com>
2017-02-23 03:01:19 +0000

    AU: 4 updated - imagemagick imagemagick.app imagemagick.tool imagemagick.tool.legacy [skip ci] https://gist.github.com/fd3d636b9b4617772f9b99ceb88838fa/74cb90cdc890dacd97225a9ac7fd4b7f62f304e3

--- automatic/imagemagick.tool.legacy/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool.legacy/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.7-8-portable-Q16-x64.zip'
-  url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.7-8-portable-Q16-x86.zip'
-  checksum       = '6b7b8379dccdc3600357755800458eb2a7c511ad6b679338e6bb1e126c4dbd5a'
-  checksum64     = '22b7d3772827d7ba7f406d4ca96ab1bfa7b34e6e717cddcdbfc9b2d88c2406cb'
+  url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.7-9-portable-Q16-x86.zip'
+  url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.7-9-portable-Q16-x64.zip'
+  checksum       = 'b8dd70946744017e89b1639e1b446fd8b31181af99c7a905cef30ad8830cac11'
+  checksum64     = '85bce3a4bac13dcbc6840f33ff633a52b1e473d4558bc4f5e4b2053ad27a6749'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
