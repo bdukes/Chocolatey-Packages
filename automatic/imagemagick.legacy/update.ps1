0d524484b77eb5c9e8828d5ea6e171e7a12d3e42
Brian Dukes <bdukes@engagesoftware.com>
2017-05-12 16:45:04 -0500

    Fix imagemagick.legacy update script

--- automatic/imagemagick.legacy/update.ps1
+++ automatic/imagemagick.legacy/update.ps1
@@ -13,8 +13,8 @@ function global:au_GetLatest {
 
     $re32 = "^http.+ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-x86-dll.exe$"
     $re64 = "^http.+ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-x64-dll.exe$"
-    $url32 = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $url64 = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $url32 = $download_page.links | ? href -match $re32 | select -First 1 -expand href
+    $url64 = $download_page.links | ? href -match $re64 | select -First 1 -expand href
 
     $versionMatch = $url64 | select-string -Pattern $re64
     $version = $versionMatch.Matches[0].Groups[1].Value -replace '-', '.'
