9df79ba8353c5a8f92f43166b7b0442133323dfe
Brian Dukes <bdukes@engagesoftware.com>
2017-11-13 08:34:08 -0600

    Update checksum for spacesniffer 1.3.0.2

--- manual/spacesniffer/tools/chocolateyinstall.ps1
+++ manual/spacesniffer/tools/chocolateyinstall.ps1
@@ -2,16 +2,16 @@
 $ErrorActionPreference = 'Stop';
 
 
-$packageName= 'spacesniffer'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://www.uderzo.it/main_products/space_sniffer/files/spacesniffer_1_3_0_2.zip'
+$packageName = 'spacesniffer'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://www.uderzo.it/main_products/space_sniffer/files/spacesniffer_1_3_0_2.zip'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  url           = $url
-  checksum      = 'ea861ae1e7273b411d9da7630b80738bd5f0fab3'
-  checksumType  = 'sha1'
+    packageName   = $packageName
+    unzipLocation = $toolsDir
+    url           = $url
+    checksum      = '156C45CB53A283D0B919CA8B1E0FAF3B967C896B8A580B962E493A27C7F465C2'
+    checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
