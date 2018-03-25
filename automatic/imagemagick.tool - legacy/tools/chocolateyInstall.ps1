a9f9fe824a9e29efcf76deee91aa95af17ba52f4
Brian Dukes (via AppVeyor) <bdukes@engagesoftware.com>
2018-03-25 02:59:31 +0000

    AU: 5 updated - imagemagick imagemagick imagemagick.app imagemagick.tool lastpass-for-applications [skip ci]

--- automatic/imagemagick.tool - legacy/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool - legacy/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.9-38-portable-Q16-x86.zip'
-  url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.9-38-portable-Q16-x64.zip'
-  checksum       = 'b54fbf746646b1252c825edb0b1c9e517c1b39fa491f5e6e706d67167272b359'
-  checksum64     = 'ecad2c980a3d8254a14d98310d1fc69ff7156134f6197377752c08a2a26ee671'
+  url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.9-39-portable-Q16-x86.zip'
+  url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.9-39-portable-Q16-x64.zip'
+  checksum       = '741530568f6f0ab8047cbc3f565981be60f9b640a0b15b490cc2d50e5c2d155f'
+  checksum64     = '846cfcbe09f7c5a02e781fec84a425a42df7e4d333bad675d0a24a8410dc8466'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
