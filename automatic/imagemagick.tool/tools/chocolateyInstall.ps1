436a1e707aad3bf5e6f2d1480bde1f52b6f7eb74
Brian Dukes (via AppVeyor) <bdukes@engagesoftware.com>
2018-03-29 16:30:37 +0000

    AU: 4 updated - imagemagick imagemagick imagemagick.app imagemagick.tool [skip ci]

--- automatic/imagemagick.tool/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.7-6-portable-Q16-x86.zip'
-  url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.7-6-portable-Q16-x64.zip'
-  checksum       = '26c507cedacc067cffbf572677b7d991cccfa231b990f8fd742fe83e88816bd1'
-  checksum64     = '2f5835951d7bae6ce4af98f888d5cc555000fcca99ebbbff6964e6c66641bbde'
+  url            = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.7-28-portable-Q16-x86.zip'
+  url64          = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.7-28-portable-Q16-x64.zip'
+  checksum       = '8bb0d4a8e8b72e2df2ba7707c53f5288dd71efe4ff51e7b6999139aa331e752c'
+  checksum64     = '8c4d0babd6d2f1684ad314650252b025e64f0467210fd8fdb86a5e2066e2c44c'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
