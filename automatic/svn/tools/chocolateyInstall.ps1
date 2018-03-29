1e408867e16d73530a971974cf9f72e4b95e7b0f
Brian Dukes <bdukes@engagesoftware.com>
2018-03-29 11:15:24 -0500

    Make svn package automatic

---
+++ automatic/svn/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'svn'
+  fileType      = 'msi'
+  url           = 'https://sourceforge.net/projects/win32svn/files/1.8.15/Setup-Subversion-1.8.15.msi'
+  silentArgs    = '/quiet'
+  validExitCodes= @(0)
+  softwareName  = 'Subversion*'
+  checksum      = ''
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+$installDir = (Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\App Paths\svn.exe').Path
+Install-ChocolateyPath $installDir
