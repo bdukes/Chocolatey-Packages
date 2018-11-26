59ada31c17f7ed9925a3a150778ad00205e85509
Brian Dukes <bdukes@engagesoftware.com>
2018-11-26 11:55:37 -0600

    hg 4.8

--- automatic/hg/tools/chocolateyInstall.ps1
+++ automatic/hg/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'hg'
   installerType  = 'exe'
-  url            = 'https://www.mercurial-scm.org/release/windows/Mercurial-4.7.exe'
-  url64          = 'https://www.mercurial-scm.org/release/windows/Mercurial-4.7-x64.exe'
-  checksum       = '62b517e5a609be8614d8bad728e0d35783af82c18ade371c24ef6486c81cd8b1'
+  url            = 'https://www.mercurial-scm.org/release/windows/Mercurial-4.8.exe'
+  url64          = 'https://www.mercurial-scm.org/release/windows/Mercurial-4.8-x64.exe'
+  checksum       = '2bf36cbd60af21600d9a296b8ec2d00131cbd92acbaf56511cb0d7de6e2a5910'
   checksumType   = 'sha256'
-  checksum64     = 'fc9846057479936093f6bae68e476faa74604dcbc66560c33186a7f82aa11137'
+  checksum64     = '21812cf412b13cbb6f1ac0ca96ca047a9bdbfc2bcfb9d44cc6d85b16572f4837'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0, 3010)
