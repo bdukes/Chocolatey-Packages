61abedcfea7c1e4eedb77c60a1ad247f1bac5aa5
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2019-03-07 17:45:45 +0000

    AU: 4 updated - imagemagick imagemagick.tool lastpass lastpass-for-applications [skip ci]

--- automatic/imagemagick.tool/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.8-24-portable-Q16-x86.zip'
-  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.8-24-portable-Q16-x64.zip'
-  checksum       = 'de62a2747e6c8685aa8f41fc5271a4c0bf645991f4b3236a4a67ac48c49e11e5'
-  checksum64     = 'b0c63924fa2c448b1e4b5a3e3dcaa97d4a975969b4fe3577e628b358db87f953'
+  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.8-32-portable-Q16-x86.zip'
+  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.8-32-portable-Q16-x64.zip'
+  checksum       = '2a38e062f6bd892ba86812ca19c1152092756e7f856d79bbe1c965462f2dc637'
+  checksum64     = 'f000c5092060ded58fb185d8b3354fb1967e998251859e47ae0f0b470072cbef'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
