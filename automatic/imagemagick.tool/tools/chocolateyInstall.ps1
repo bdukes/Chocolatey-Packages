492cb90de699bcb3caf4d199c48701a254b472bd
Brian Dukes <bdukes@engagesoftware.com>
2019-06-03 09:05:35 -0500

    imagemagick 7.0.8.48, lastpass-for-applications 4.28.0

--- automatic/imagemagick.tool/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.8-45-portable-Q16-x86.zip'
-  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.8-45-portable-Q16-x64.zip'
-  checksum       = '2835d76e2f42f8d45a29f40551a50b5d50119d5413c60f72b6da7bf56d71dfa1'
-  checksum64     = '8e89d68f7cb1437845ceaa071166fce2495f6dfe15215d8b854cd22bed750b2e'
+  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.8-48-portable-Q16-x86.zip'
+  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.8-48-portable-Q16-x64.zip'
+  checksum       = 'd19a3449e7231f2fb756a9a7fcfe1bfc8c128648f931bc370339b9cc62cead9c'
+  checksum64     = '1e9fb3c7dcfbcf588713d8af4911afc034dee718a5ca9a54cf6bae1b242c9d8a'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
