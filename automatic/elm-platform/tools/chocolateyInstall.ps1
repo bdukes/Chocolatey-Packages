84cd81625cff1a3200797c97f92f858a0b370a6b
Brian Dukes <bdukes@engagesoftware.com>
2019-10-22 09:13:57 -0500

    elm-platform 0.19.1

--- automatic/elm-platform/tools/chocolateyInstall.ps1
+++ automatic/elm-platform/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName   = 'elm-platform'
   fileType      = 'EXE'
-  url           = 'https://github.com/elm/compiler/releases/download/0.19.0/installer-for-windows.exe'
+  url           = 'https://github.com/elm/compiler/releases/download/0.19.1/installer-for-windows.exe'
   silentArgs    = '/S'
   validExitCodes= @(0)
   softwareName  = 'Elm Platform*'
-  checksum      = 'b7139002a7f348c0428da6f47db551642f9c5d8a178a08e5b5a426a9a1d8a7d9'
+  checksum      = '02947D00090F556ABCF8F8E95997AC993825BD783ABF05786AFE8F60A002D496'
   checksumType  = 'sha256'
 }
 
