eefa4dbb31067c06ca65514442c2915511534780
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2019-11-16 11:20:55 +0000

    AU: 3 updated - imagemagick imagemagick imagemagick.tool [skip ci]

--- automatic/imagemagick.tool/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.9-2-portable-Q16-x86.zip'
-  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.9-2-portable-Q16-x64.zip'
-  checksum       = '55b43158f8cd97f5d9678964eec1341b8c5192fd5f5597a7d74b17d963a707b7'
-  checksum64     = '67e1a63537d02197eeebfce2bd1eb16c9e1bc6058bde689d932205fa4f41c762'
+  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.9-4-portable-Q16-x86.zip'
+  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.9-4-portable-Q16-x64.zip'
+  checksum       = '1c3fe9800af473d6d4ddf829899df8c2a862f906d523efdb56f4133dc7b4ddae'
+  checksum64     = 'e753744a810f7b8eb119f41ed4aa973c35bd6caeecf6a1ffa2f472310e1247b3'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
