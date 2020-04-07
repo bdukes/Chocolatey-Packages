f4465508c9da8e8985b427baf6103566ff9b7d1e
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2020-04-07 10:26:57 +0000

    AU: 5 updated - imagemagick imagemagick imagemagick.app imagemagick.tool imagemagick.tool [skip ci]

--- automatic/imagemagick.tool - legacy/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool - legacy/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://imagemagick.org/download/binaries/ImageMagick-6.9.11-5-portable-Q16-x86.zip'
-  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-6.9.11-5-portable-Q16-x64.zip'
-  checksum       = 'e05e3db8f8eabd1d2d08018f54e56cbb4fc2f1a7cba8c56f1ed60f001a0b85bc'
-  checksum64     = '03121d9f4b2c9773479dbac31dca90fc3e7efa5453cdcad23e6fe0dbb41e4c3a'
+  url            = 'https://imagemagick.org/download/binaries/ImageMagick-6.9.11-6-portable-Q16-x86.zip'
+  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-6.9.11-6-portable-Q16-x64.zip'
+  checksum       = 'a4508acf2b031332334f3d6e4903b8923020bdcdbfc65c654cb2fc1d79cad2c8'
+  checksum64     = '482b154eae2b57e13b7655ba97d24213b6532554b17b43dd678cc15c62964183'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
--- automatic/imagemagick.tool/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.10-5-portable-Q16-x86.zip'
-  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.10-5-portable-Q16-x64.zip'
-  checksum       = '01e0a3fd3fcd76f4e7ffd40dfc0db0b4dcd27a6fa6e5bbf24048ff0d45e4debe'
-  checksum64     = '361e848c9b860c8986f8370b0773eda8ace6082ce9ad898a5189fc1f6708946f'
+  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.10-6-portable-Q16-x86.zip'
+  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.10-6-portable-Q16-x64.zip'
+  checksum       = '564fa4b1e38d490a28daf179a41eb722bd57e4237d1144dc312e39d4202aa2e7'
+  checksum64     = '1fd6a0ce3b8c8e58e45888d5570b82813d3f4ecffc846e780fe7ea909e6f056e'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
