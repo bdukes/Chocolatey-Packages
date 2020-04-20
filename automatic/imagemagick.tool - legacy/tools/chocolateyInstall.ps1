0a3b93ff9edc67ffc4bcba38f1db892deec2b359
Brian Dukes <bdukes@engagesoftware.com>
2020-04-20 10:49:37 -0500

    imagemagick 7.0.10-7 and 6.9.11-7

--- automatic/imagemagick.tool - legacy/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool - legacy/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://imagemagick.org/download/binaries/ImageMagick-6.9.11-6-portable-Q16-x86.zip'
-  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-6.9.11-6-portable-Q16-x64.zip'
-  fallbackUrl    = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-6.9.11-6-portable-Q16-x86.zip'
-  fallbackUrl64  = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-6.9.11-6-portable-Q16-x64.zip'
-  checksum       = 'a4508acf2b031332334f3d6e4903b8923020bdcdbfc65c654cb2fc1d79cad2c8'
-  checksum64     = '482b154eae2b57e13b7655ba97d24213b6532554b17b43dd678cc15c62964183'
+  url            = 'https://imagemagick.org/download/binaries/ImageMagick-6.9.11-7-portable-Q16-x86.zip'
+  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-6.9.11-7-portable-Q16-x64.zip'
+  fallbackUrl    = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-6.9.11-7-portable-Q16-x86.zip'
+  fallbackUrl64  = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-6.9.11-7-portable-Q16-x64.zip'
+  checksum       = '29adb08744c8c296bf18e8ee38399eb4e9851d18e1762411f64fa414ab2b46fc'
+  checksum64     = '1f76ffac2fe9d6a4c52f84684b0ba3ee2d9a5d852a7ad6b7db3568a2e2c1e44e'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
--- automatic/imagemagick.tool/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.10-6-portable-Q16-x86.zip'
-  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.10-6-portable-Q16-x64.zip'
-  fallbackUrl    = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.10-6-portable-Q16-x86.zip'
-  fallbackUrl64  = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.10-6-portable-Q16-x64.zip'
-  checksum       = '564fa4b1e38d490a28daf179a41eb722bd57e4237d1144dc312e39d4202aa2e7'
-  checksum64     = '1fd6a0ce3b8c8e58e45888d5570b82813d3f4ecffc846e780fe7ea909e6f056e'
+  url            = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.10-7-portable-Q16-x86.zip'
+  url64          = 'https://imagemagick.org/download/binaries/ImageMagick-7.0.10-7-portable-Q16-x64.zip'
+  fallbackUrl    = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.10-7-portable-Q16-x86.zip'
+  fallbackUrl64  = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.10-7-portable-Q16-x64.zip'
+  checksum       = '94e8684e7b963f1f3278de5272b1a71e74fc19f231f79c6f8f59c164efe94fa5'
+  checksum64     = 'a505a3c5379be86858e23f31b5f95e79e42443c2e541d806f66eaf19710646a2'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
