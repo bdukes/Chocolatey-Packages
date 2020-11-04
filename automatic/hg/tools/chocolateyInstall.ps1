c3a30a7e045c08b15a9aed9275373735d77abd76
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2020-11-04 11:05:13 +0000

    AU: 1 updated - hg [skip ci]

--- automatic/hg/tools/chocolateyInstall.ps1
+++ automatic/hg/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName    = 'hg'
   installerType  = 'exe'
-  url            = 'https://www.mercurial-scm.org/release/windows/Mercurial-5.5.2-x86-python2.exe'
-  url64          = 'https://www.mercurial-scm.org/release/windows/Mercurial-5.5.2-x64-python2.exe'
-  checksum       = 'aeb0746839d075d04b113b680da21cb0a93a2d86f8822c9bde768fc71d8fb756'
+  url            = 'https://www.mercurial-scm.org/release/windows/Mercurial-5.6-x86-python2.exe'
+  url64          = 'https://www.mercurial-scm.org/release/windows/Mercurial-5.6-x64-python2.exe'
+  checksum       = 'c30bde65569b1b72a58fc498270f98de64938cee3ef588e7e57740f34a4dd779'
   checksumType   = 'sha256'
-  checksum64     = 'cd2b6ea421556d9cdc51f951710fd80815498ec01963295b27f6db5a4f46e862'
+  checksum64     = '9f826cdc88ebdd5b7e2456fbd009ceeb08de371c31744940b009c256c5fda555'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0, 3010)
