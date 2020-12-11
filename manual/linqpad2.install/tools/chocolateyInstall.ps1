f2924f8ac5c5196ce9fb4b0733bb1e672b30e49d
Brian Dukes <bdukes@engagesoftware.com>
2020-12-11 10:34:50 -0600

    Update LINQPad 2

--- manual/linqpad2.install/tools/chocolateyInstall.ps1
+++ manual/linqpad2.install/tools/chocolateyInstall.ps1
@@ -1,20 +1,18 @@
-﻿Install-ChocolateyPackage 'linqpad2.install' 'exe' '/silent' 'https://www.linqpad.net/GetFile.aspx?LINQPadSetup.exe'
+﻿$ErrorActionPreference = 'Stop';
 
-$ErrorActionPreference = 'Stop';
-
-$packageName= 'linqpad2.install'
-$url        = 'https://www.linqpad.net/GetFile.aspx?LINQPadSetup.exe'
+$packageName = 'linqpad2.install'
+$url = 'https://www.linqpad.net/GetFile.aspx?LINQPadSetup.exe'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  softwareName  = 'linqpad*'
-  checksum      = ''
-  checksumType  = 'sha256'
-  silentArgs    = "/silent"
-  validExitCodes= @(0)
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'linqpad*'
+  checksum       = '0B407603106679BC128292D3410E3F00C5F16FD72D928414D12C1142FC853979'
+  checksumType   = 'sha256'
+  silentArgs     = "/silent"
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
