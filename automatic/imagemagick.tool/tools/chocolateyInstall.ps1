04a2f6c8d7ccd9622e4ede7c5907d2596f2ebd9e
mimmiechan <57503195+mimmiechan@users.noreply.github.com>
2021-02-13 21:45:55 +0100

    Update chocolateyInstall.ps1

--- automatic/imagemagick.tool/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.10-62-portable-Q16-x86.zip'
-  url64          = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.10-62-portable-Q16-x64.zip'
-  fallbackUrl    = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.10-62-portable-Q16-x86.zip'
-  fallbackUrl64  = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.10-62-portable-Q16-x64.zip'
+  url            = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.11-0-portable-Q16-x86.zip'
+  url64          = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.11-0-portable-Q16-x64.zip'
+  fallbackUrl    = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.11-0-portable-Q16-x86.zip'
+  fallbackUrl64  = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.11-0-portable-Q16-x64.zip'
   checksum       = '5044ad6b1dc77f52a526ea6cc65d301ff8ccb4481476fe7af26aa29f611be8ce'
   checksum64     = 'f0b085637e57714ac9ae4049544ff10df6139678c5bbc6e2b1d1b30ecbfc03ee'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
