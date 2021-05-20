01b4e3914b97c72a3e2e2e03b21088f5bcbe0175
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2021-05-20 10:09:05 +0000

    AU: 1 updated - imagemagick.tool [skip ci] https://gist.github.com/fd3d636b9b4617772f9b99ceb88838fa

--- automatic/imagemagick.tool/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.tool/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageArgs = @{
   packageName    = 'imagemagick.tool'
-  url            = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.11-12-portable-Q16-x86.zip'
-  url64          = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.11-12-portable-Q16-x64.zip'
-  fallbackUrl    = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.11-12-portable-Q16-x86.zip'
-  fallbackUrl64  = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.11-12-portable-Q16-x64.zip'
-  checksum       = '7f4f58bcfd561e4cd5ab626cf4dbdcc4c5a3de3dde1d1e4c0be4034790e7e563'
-  checksum64     = 'bb1a2a4cbdec46c082d1bbbdede69f3e81a38722d662c779666c2e9c95353fea'
+  url            = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.11-13-portable-Q16-x86.zip'
+  url64          = 'https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.0.11-13-portable-Q16-x64.zip'
+  fallbackUrl    = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.11-13-portable-Q16-x86.zip'
+  fallbackUrl64  = 'https://ftp.icm.edu.pl/pub/graphics/ImageMagick/binaries/ImageMagick-7.0.11-13-portable-Q16-x64.zip'
+  checksum       = '329bd39399dc04af4931cbf6b4fa73b89f20297a05559f36c751ac47a984690f'
+  checksum64     = 'd703b8a9762d2fafad5e627467682053e3e4ee15cec49243bfe4a3280e744902'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
