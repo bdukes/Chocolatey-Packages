08c0842da085c130494b35364e45899559e36b8f
Brian Dukes <bdukes@engagesoftware.com>
2022-04-08 13:17:21 -0500

    Untar mob.exe

--- automatic/mob/tools/chocolateyInstall.ps1
+++ automatic/mob/tools/chocolateyInstall.ps1
@@ -1,11 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition;
+
 $packageArgs = @{
   packageName   = 'mob'
   url           = 'https://github.com/remotemobprogramming/mob/releases/download/v3.0.0/mob_v3.0.0_windows_amd64.tar.gz'
   checksum      = 'fb94ef338d44de002653818dcb52504906161561713d4d0430061fb68c888d22'
   checksumType  = 'sha256'
-  unzipLocation = (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+  unzipLocation = $toolsDir
 }
 
 Install-ChocolateyZipPackage @packageArgs;
+
+$tarFile = Get-Item "$toolsDir/*.tar";
+
+Get-ChocolateyUnzip -FileFullPath $tarFile -Destination $toolsDir -PackageName $packageArgs.packageName;
+
+Remove-Item $tarFile;
