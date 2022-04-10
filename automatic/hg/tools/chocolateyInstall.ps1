79a9d9497462a6844f7420e2167ec033607d83d1
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2022-04-10 10:06:34 +0000

    AU: 2 updated - hg linqpad5 [skip ci] https://gist.github.com/fd3d636b9b4617772f9b99ceb88838fa/65f9d1bafd696a426026bc439e164378207eaf6d

--- automatic/hg/tools/chocolateyInstall.ps1
+++ automatic/hg/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
     packageName    = 'hg'
     installerType  = 'exe'
-    url            = 'https://www.mercurial-scm.org/release/windows/Mercurial-6.1-x86.exe'
-    url64          = 'https://www.mercurial-scm.org/release/windows/Mercurial-6.1-x64.exe'
-    checksum       = 'd204d0a262476e1fa888439bd2a710c3bba3b8143945c7ba125afe8f3d7fac57'
+    url            = 'https://www.mercurial-scm.org/release/windows/Mercurial-6.1.1-x86.exe'
+    url64          = 'https://www.mercurial-scm.org/release/windows/Mercurial-6.1.1-x64.exe'
+    checksum       = '53abdf619faccf5984e557d8266b3965e1286a469af3a302a77b7fd3ee1091f0'
     checksumType   = 'sha256'
-    checksum64     = '4f648beb2706f7bd199596a99ba2486fd17a86f815f44fc1a7d44ae7084c1299'
+    checksum64     = 'cb60c00a900fea9045eed88e47fb03628bd716b6385bad8e7f86579e990ea625'
     checksumType64 = 'sha256'
     silentArgs     = '/VERYSILENT'
     validExitCodes = @(0, 3010)
