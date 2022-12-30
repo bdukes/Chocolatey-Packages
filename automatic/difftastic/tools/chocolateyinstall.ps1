afa0361de94f1c3cbbaa586b02b98745829b455b
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2022-12-30 11:09:04 +0000

    AU: 2 updated - difftastic imagemagick.tool [skip ci] https://gist.github.com/fd3d636b9b4617772f9b99ceb88838fa/42f87dba510cbdff22ddf7b7540246ba757e71b3

--- automatic/difftastic/tools/chocolateyinstall.ps1
+++ automatic/difftastic/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition;
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/Wilfred/difftastic/releases/download/0.39.0/difft-x86_64-pc-windows-msvc.zip'
-  checksum      = '915e3992947254826d895547c9c2468be0e9f7afd080a904eb0ab894905c315f'
+  url           = 'https://github.com/Wilfred/difftastic/releases/download/0.40.0/difft-x86_64-pc-windows-msvc.zip'
+  checksum      = 'c1e4ad14017c70cbbab95ab810dfcae8420f668e8f4f56793f57565e8d490138'
   checksumType  = 'sha256'
 }
 
