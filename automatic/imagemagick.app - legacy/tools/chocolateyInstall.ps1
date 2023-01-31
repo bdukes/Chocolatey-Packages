1127f85e47efcf247035c7de660e4625433e1018
T3ck <timothee.fourel@univ-lorraine.fr>
2023-01-31 11:09:58 +0100

    Patch Uninstall of older version

--- automatic/imagemagick.app - legacy/tools/chocolateyInstall.ps1
+++ automatic/imagemagick.app - legacy/tools/chocolateyInstall.ps1
@@ -26,20 +26,16 @@ if ($env:chocolateyPackageParameters) {
     }
 }
 
-try {
-    # Uninstall older version of imagemagick, otherwise the installation won’t be silent.
-    $regPath = 'HKLM:\SOFTWARE\ImageMagick\Current';
-    if ($env:chocolateyForceX86) {
-        $regPath = 'HKLM:\SOFTWARE\Wow6432Node\ImageMagick\Current';
-    }
-    if (Test-Path $regPath) {
-        $uninstallPath = (Get-ItemProperty -Path $regPath).BinPath;
-        $uninstallFilePath = "$uninstallPath\unins000.exe";
-        Uninstall-ChocolateyPackage $packageArgs.packageName $packageArgs.installerType $packageArgs.silentArgs $uninstallFilePath;
-    }
-} catch {
-    Write-Warning "$packageName uninstallation failed, with message $($_.Exception.Message)";
-    Write-Warning "$packageName installation may not be silent";
+#Uninstall old version if present
+$UninstallKey = $(Get-UninstallRegistryKey -softwareName $packageArgs.softwareName).UninstallString
+
+if ($UninstallKey.count -eq 1) {
+    # foreach ($key in $UninstallKey){
+        Write-Host "Uninstalling previous version is required for silent upgrade"
+        Uninstall-ChocolateyPackage $packageArgs.packageName $packageArgs.installerType $packageArgs.silentArgs $UninstallKey
+    # }
+}elseif ($UninstallKey.count -gt 1) {
+    throw "Uninstallation of the old version was aborted because they are too much uninstall entries. List of entries : $UninstallKey"
 }
 
 Write-Verbose "Installing with arguments: $($packageArgs.silentArgs)";
