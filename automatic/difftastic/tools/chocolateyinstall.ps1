c26429cb1dee93d3f413201e67cb647440d53b2b
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2023-02-09 11:10:01 +0000

    AU: 1 updated - difftastic [skip ci] https://gist.github.com/fd3d636b9b4617772f9b99ceb88838fa/e5f2ed97d8b3f3d7e6bd10ef102e6f7c55158530

--- automatic/difftastic/tools/chocolateyinstall.ps1
+++ automatic/difftastic/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition;
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/Wilfred/difftastic/releases/download/0.42.0/difft-x86_64-pc-windows-msvc.zip'
-  checksum      = 'aa45d045883644a2e817394a42041f5be4aebc1a58f87dbdf2f65d690f71f2d9'
+  url           = 'https://github.com/Wilfred/difftastic/releases/download/0.43.1/difft-x86_64-pc-windows-msvc.zip'
+  checksum      = 'ffc9cf544cff273d56e68d8c2a7760c646e659bfb5096c99a1265a226f602dec'
   checksumType  = 'sha256'
 }
 
