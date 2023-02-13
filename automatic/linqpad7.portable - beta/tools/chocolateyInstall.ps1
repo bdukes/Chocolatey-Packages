182abb4f6f7ad99d6868be62c35999b56cfc6ab5
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2023-02-13 11:23:47 +0000

    AU: 5 updated - imagemagick imagemagick imagemagick.app imagemagick.app linqpad7.portable [skip ci] https://gist.github.com/fd3d636b9b4617772f9b99ceb88838fa/6149baf68af4ed5797ba8473538e763499fd50ec

--- automatic/linqpad7.portable - beta/tools/chocolateyInstall.ps1
+++ automatic/linqpad7.portable - beta/tools/chocolateyInstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url           = $url
-  checksum      = '0b1098a284ed76af2a70ed37eba21a365df1d21f3b819da01956b3d97dc81d5b'
+  checksum      = '525f810c098f6a9d356741ff534c1f1bac52125d2c62712a13d3d8fe38a39f1a'
   checksumType  = 'sha256'
 }
 
