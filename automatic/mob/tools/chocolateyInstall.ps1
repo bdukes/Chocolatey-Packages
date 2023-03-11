1e26d89635e1dae54c21f1b187ca13ac94ac7cb7
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2023-03-11 11:06:13 +0000

    AU: 2 updated - imagemagick mob [skip ci] https://gist.github.com/fd3d636b9b4617772f9b99ceb88838fa/40d549b03cc8f9bd8ed664968e755297a9a0c90b

--- automatic/mob/tools/chocolateyInstall.ps1
+++ automatic/mob/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition;
 
 $packageArgs = @{
   packageName   = 'mob'
-  url           = 'https://github.com/remotemobprogramming/mob/releases/download/v4.3.0/mob_v4.3.0_windows_amd64.tar.gz'
-  checksum      = '69cc9f8a38ca60d7d6f9f28ab79c78c54a8c227b80884193432c745064b53fb8'
+  url           = 'https://github.com/remotemobprogramming/mob/releases/download/v4.3.1/mob_v4.3.1_windows_amd64.tar.gz'
+  checksum      = '8611e9b676b41602a146d05b7a5c3467a485240788edb54ef9604772af6c4a20'
   checksumType  = 'sha256'
   unzipLocation = $toolsDir
 }
