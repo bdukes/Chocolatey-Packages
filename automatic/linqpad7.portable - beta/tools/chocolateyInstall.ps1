9ab250fa0b0416204c89ae087236620b3d3abfbd
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2023-04-19 10:07:14 +0000

    AU: 1 updated - linqpad7.portable [skip ci] https://gist.github.com/bdukes/fd3d636b9b4617772f9b99ceb88838fa/94a1018fc4077e3f42206399a4b45ef285a136c0

--- automatic/linqpad7.portable - beta/tools/chocolateyInstall.ps1
+++ automatic/linqpad7.portable - beta/tools/chocolateyInstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url           = $url
-  checksum      = 'd685f0aa20fd56849a55b6901209d99b6f3320cf0a2843171847d1082b4f4e85'
+  checksum      = '7d56c1d8b875558eb581db009710ae4d3b0aded9d550662c0dc9211e0110f8c9'
   checksumType  = 'sha256'
 }
 
