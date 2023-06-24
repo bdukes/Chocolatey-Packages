5e337b16a7e4d3293f87765afd5f0a61c9dc5bd5
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2023-06-24 10:04:35 +0000

    AU: 1 updated - mob [skip ci] https://gist.github.com/bdukes/fd3d636b9b4617772f9b99ceb88838fa/9625d477fb9e348eb050b3fc345e22d10ffdac83

--- automatic/mob/tools/chocolateyInstall.ps1
+++ automatic/mob/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition;
 
 $packageArgs = @{
   packageName   = 'mob'
-  url           = 'https://github.com/remotemobprogramming/mob/releases/download/v4.4.3/mob_v4.4.3_windows_amd64.tar.gz'
-  checksum      = 'a806ed015869c34addf6bc82b439b488c0aa4e936e3cb907ff9c1117e93989e7'
+  url           = 'https://github.com/remotemobprogramming/mob/releases/download/v4.4.5/mob_v4.4.5_windows_amd64.tar.gz'
+  checksum      = 'ab304705dde2b2cb8d1d9c9314533ab32d61c60ecfeeb51e53aca27138c1f436'
   checksumType  = 'sha256'
   unzipLocation = $toolsDir
 }
