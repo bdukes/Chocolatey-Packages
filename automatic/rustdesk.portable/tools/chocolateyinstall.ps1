859ed08f5105a8f68387d0da268205be13d99c0f
Brian Dukes <bdukes@engagesoftware.com>
2023-07-22 15:40:01 -0500

    rustdesk.portable 1.2.1

--- automatic/rustdesk.portable/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.portable/tools/chocolateyinstall.ps1
@@ -1,16 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition;
+$exeFile = Join-Path $toolsDir 'rustdesk.exe';
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-  fileFullPath = $toolsDir
-  url          = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.0/rustdesk-1.2.0-x86_64.exe'
-  checksum     = 'e36c78e042c98ee4c5e6030e162b87ecb64f492c948caf2266f7d9397d73cbc5'
+  fileFullPath = $exeFile
+  url          = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.1/rustdesk-1.2.1-x86_64.exe'
+  checksum     = 'd5ca8a2127e4803d0a742501ee10f6aac6569b81721333ea557e874dfa1a3f47'
   checksumType = 'sha256'
 }
 
 Get-ChocolateyWebFile @packageArgs;
-
-$exeFile = Get-Item "$toolsDir/*.exe";
-New-Item -Path ($exeFile.FullName + '.ignore') -ItemType File;
-Move-Item $exeFile (Join-Path $toolsDir 'rustdesk.exe') -Force;
