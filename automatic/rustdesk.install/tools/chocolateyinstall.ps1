a77aa27867f5730666df9c9c54d5b698aa34668e
Brian Dukes <bdukes@engagesoftware.com>
2023-07-22 15:52:08 -0500

    Add missing checksum for RustDesk 1.2.0

--- automatic/rustdesk.install/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.install/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   url            = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.0/rustdesk-1.2.0-x86_64.exe'
   silentArgs     = "--silent-install"
   validExitCodes = @(0)
-  checksum       = ''
-  checksumType   = ''
+  checksum       = 'e36c78e042c98ee4c5e6030e162b87ecb64f492c948caf2266f7d9397d73cbc5'
+  checksumType   = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs;
