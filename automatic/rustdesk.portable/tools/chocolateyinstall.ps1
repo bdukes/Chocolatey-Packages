295d4917f2d25428f628e38548bd198abf0f1e6b
Brian Dukes <bdukes@engagesoftware.com>
2023-08-23 09:15:03 -0500

    rustdesk 1.2.2.20230823

--- automatic/rustdesk.portable/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.portable/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   fileFullPath = $exeFile
   url          = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.2/rustdesk-1.2.2-x86_64.exe'
-  checksum     = 'c588654739f6a19adff00fc67550ac9da2e7c7de94009bdea01fd94300d76dcf'
+  checksum     = '7D8790E65A906706A93734B91EFA6DFDB732F9897E04707233FE48033BD5654E'
   checksumType = 'sha256'
 }
 
