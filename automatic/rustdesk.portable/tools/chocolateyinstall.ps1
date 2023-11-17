aa01c75fd9a77c87e6e9dcc4cbb0749b486b5738
Darshaka Pathirana <dpat@syn-net.org>
2023-11-17 14:37:02 +0100

    Fix checksum for rustdesk 1.2.3
    The checksum was calculated like this:
    
      ❯ curl -sL https://github.com/rustdesk/rustdesk/releases/download/1.2.3/rustdesk-1.2.3-x86_64.exe | sha256sum
      23b661d7bc171cd500d5096456905283ffe06479582b62d3bd5066633935d43e  -
    
    Closes: bdukes/Chocolatey-Packages#51

--- automatic/rustdesk.portable/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.portable/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   fileFullPath = $exeFile
   url          = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.3/rustdesk-1.2.3-x86_64.exe'
-  checksum     = '0856e5c8c3ee94edfddb80be160ee6857ab72d972065be86783af2c907a7cd54'
+  checksum     = '23b661d7bc171cd500d5096456905283ffe06479582b62d3bd5066633935d43e'
   checksumType = 'sha256'
 }
 
