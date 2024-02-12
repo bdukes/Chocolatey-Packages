ba821de7f9757f39f1b1e6cddddd1b2b5036c939
Chris Blyth <blythmeister@gmail.com>
2024-02-12 12:04:21 +0000

    Add base linqpad8 packages

---
+++ automatic/linqpad8.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'linqpad8.install'
+$url = 'https://www.linqpad.net/GetFile.aspx?LINQPad8Setup.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'linqpad*'
+  checksum       = '2eeb973b082f8c8005fa7feb86a5c10c2276fa6922ddc203d56540d9e4bb821d'
+  checksumType   = 'sha256'
+  silentArgs     = "/verysilent"
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+Install-ChocolateyPath "${env:ProgramFiles}\LinqPad8"
---
+++ automatic/linqpad8/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿#Install-VirtualPackage 'linqpad8.install' 'linqpad8.portable'
