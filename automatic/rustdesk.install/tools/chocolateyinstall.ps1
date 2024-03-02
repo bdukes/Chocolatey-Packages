780fe86b031afac72ad56951e04ad72b14ed4179
Brian Dukes <bdukes@engagesoftware.com>
2024-03-02 14:03:08 -0600

    Update rustdesk to 1.2.3-1

--- automatic/rustdesk.install/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.install/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.3/rustdesk-1.2.3-x86_64.exe'
+  url            = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.3-1/rustdesk-1.2.3-1-x86_64.exe'
   silentArgs     = "--silent-install"
   validExitCodes = @(0)
-  checksum       = '23b661d7bc171cd500d5096456905283ffe06479582b62d3bd5066633935d43e'
+  checksum       = '4996194639c099db0d854d20832a64e6629fefa37ce6a01ffd8710ac6c9e2522'
   checksumType   = 'sha256'
 }
 
