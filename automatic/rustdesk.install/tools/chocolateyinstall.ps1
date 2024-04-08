e21caae1ea28dd8583033f61665f0f07faf2bc6a
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2024-04-08 10:08:09 +0000

    AU: 8 updated - imagemagick imagemagick imagemagick.app imagemagick.app imagemagick.tool imagemagick.tool rustdesk rustdesk.install [skip ci] https://gist.github.com/bdukes/fd3d636b9b4617772f9b99ceb88838fa/c3ab255d74101b346680bb08f1e1b45091dc8f02

--- automatic/rustdesk.install/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.install/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.3-1/rustdesk-1.2.3-1-x86_64.exe'
+  url            = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.3-2/rustdesk-1.2.3-2-x86_64.exe'
   silentArgs     = "--silent-install"
   validExitCodes = @(0)
-  checksum       = '4996194639c099db0d854d20832a64e6629fefa37ce6a01ffd8710ac6c9e2522'
+  checksum       = '04f61d92f0723aadbd6410e329197b7a3d245b8a31ae5f71224946aa83ef3db8'
   checksumType   = 'sha256'
 }
 
