089658cfaf6b56d1ff97e3a3a1c51705ef62f610
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2024-07-23 10:04:52 +0000

    AU: 3 updated - rustdesk rustdesk.install rustdesk.portable [skip ci] https://gist.github.com/bdukes/fd3d636b9b4617772f9b99ceb88838fa/42a189b66d7be91fb4b922a97dac7968ff5bd27d

--- automatic/rustdesk.install/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.install/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.6/rustdesk-1.2.6-x86_64.exe'
+  url            = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.7/rustdesk-1.2.7-x86_64.exe'
   silentArgs     = "--silent-install"
   validExitCodes = @(0)
-  checksum       = '9a0420bebf85e06fe23c974465d461d06a94a30582b0e643c1e642aebec0a952'
+  checksum       = 'd86b4dac7fd5ed52de72186246a8c61a14a7d118004eb6a7b60aec95646f7a01'
   checksumType   = 'sha256'
 }
 
--- automatic/rustdesk.portable/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.portable/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $exeFile = Join-Path $toolsDir 'rustdesk.exe';
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   fileFullPath = $exeFile
-  url          = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.6/rustdesk-1.2.6-x86_64.exe'
-  checksum     = '9a0420bebf85e06fe23c974465d461d06a94a30582b0e643c1e642aebec0a952'
+  url          = 'https://github.com/rustdesk/rustdesk/releases/download/1.2.7/rustdesk-1.2.7-x86_64.exe'
+  checksum     = 'd86b4dac7fd5ed52de72186246a8c61a14a7d118004eb6a7b60aec95646f7a01'
   checksumType = 'sha256'
 }
 
