03337eb5a46bf9c1a696487f21fd331b986cb331
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2025-02-05 11:04:17 +0000

    AU: 1 updated - linqpad8.portable [skip ci] https://gist.github.com/bdukes/fd3d636b9b4617772f9b99ceb88838fa/283b9282cc09c31071996f84a27684eaa1d7d20f

--- automatic/linqpad8.portable - beta/tools/chocolateyInstall.ps1
+++ automatic/linqpad8.portable - beta/tools/chocolateyInstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url           = $url
-  checksum      = '86a68c9d18c9ca06130218a9ed275c6c9798c5e4c7170b7bfc996ad9184e8a01'
+  checksum      = 'ec238d27a550a8de7749322945c22121dac6f42635f192b62b34ed893b0a9712'
   checksumType  = 'sha256'
 }
 
