19897799d39cc83aa7fc2fd1faf675134123b1d2
Brian Dukes <bdukes@engagesoftware.com>
2025-04-07 22:13:26 +0000

    Fix checksum for rustdesk.portable 1.3.9
    Fixes #61

--- automatic/rustdesk.portable/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.portable/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   fileFullPath = $exeFile
   url          = 'https://github.com/rustdesk/rustdesk/releases/download/1.3.9/rustdesk-1.3.9-x86_64.exe'
-  checksum     = 'df37d49857727733ccefc661d9ccb7a848a586bdac3781c976287004a8ffc417'
+  checksum     = 'd306e1634f82fd5fdadd49f06d81484b9f31ac84e61efc65d722e9cf5fe297c5'
   checksumType = 'sha256'
 }
 
