9417f496aec4021775a421867396a3a9cff692b2
Brian Dukes <bdukes@engagesoftware.com>
2025-05-15 11:22:44 -0500

    Remove svn package

--- automatic/svn/tools/chocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-
-$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-
-. $toolsDir\Uninstall-ChocolateyPath.ps1
-
-$installDir = (Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\App Paths\svn.exe').Path
-Uninstall-ChocolateyPath $installDir
