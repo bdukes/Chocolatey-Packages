82b3dee91f4118376cf659605069c23fe3f4ad45
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2025-05-15 13:40:25 -0400

    chore(rustdesk.portable) AU rewrite

--- automatic/rustdesk.portable/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.portable/tools/chocolateyinstall.ps1
@@ -1,13 +1,24 @@
-﻿$ErrorActionPreference = 'Stop';
-$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition;
-$exeFile = Join-Path $toolsDir 'rustdesk.exe';
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$exeFile               = Join-Path $toolsDir 'rustdesk.exe'
+
+$url                   = 'https://github.com/rustdesk/rustdesk/releases/download/1.4.0/rustdesk-1.4.0-x86-sciter.exe'
+$checksum              = '6abc7a882dd5d581d709534f414abdf55c11790eb4bad46dc5e86edf5fb35c33'
+$ChecksumType          = 'sha256'
+$url64                 = 'https://github.com/rustdesk/rustdesk/releases/download/1.4.0/rustdesk-1.4.0-x86_64.exe'
+$checksum64            = '118d2ca9c9388a7f781ae56c0bfe1e765e7c3272312bea5569b4cb5710d8c419'
+$ChecksumType64        = 'sha256'
 
 $packageArgs = @{
-  packageName  = $env:ChocolateyPackageName
-  fileFullPath = $exeFile
-  url          = 'https://github.com/rustdesk/rustdesk/releases/download/1.4.0/rustdesk-1.4.0-x86_64.exe'
-  checksum     = '118d2ca9c9388a7f781ae56c0bfe1e765e7c3272312bea5569b4cb5710d8c419'
-  checksumType = 'sha256'
+  packageName           = $env:ChocolateyPackageName
+  fileFullPath          = $exeFile
+  url                   = $url
+  url64                 = $url64
+  checksum              = $checksum
+  checksum64            = $checksum64
+  checksumType          = $checksumType
+  checksumType64        = $checksumType64
 }
 
-Get-ChocolateyWebFile @packageArgs;
+Get-ChocolateyWebFile @packageArgs
