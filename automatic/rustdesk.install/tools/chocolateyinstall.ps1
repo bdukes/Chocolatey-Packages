26867fd9392df1bee5ef0a86894dfb7e2ee95e2d
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2025-05-15 14:45:47 +0000

    AU: 3 updated - rustdesk rustdesk.install rustdesk.portable [skip ci] https://gist.github.com/bdukes/fd3d636b9b4617772f9b99ceb88838fa/25836c2e68c9e3403740ffd02e89def87c2b0aa2

--- automatic/rustdesk.install/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.install/tools/chocolateyinstall.ps1
@@ -10,10 +10,10 @@ if (Test-Path $installedPath) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://github.com/rustdesk/rustdesk/releases/download/1.3.9/rustdesk-1.3.9-x86_64.exe'
+  url            = 'https://github.com/rustdesk/rustdesk/releases/download/1.4.0/rustdesk-1.4.0-x86_64.exe'
   silentArgs     = "--silent-install"
   validExitCodes = @(0)
-  checksum       = 'd306e1634f82fd5fdadd49f06d81484b9f31ac84e61efc65d722e9cf5fe297c5'
+  checksum       = '118d2ca9c9388a7f781ae56c0bfe1e765e7c3272312bea5569b4cb5710d8c419'
   checksumType   = 'sha256'
 }
 
