da1f8ed270ad4b960bbe4e4cf991bcf23b99f36a
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2026-01-05 12:13:21 -0500

    chore(rustdesk.portable): update checksums

--- automatic/rustdesk.portable/tools/chocolateyinstall.ps1
+++ automatic/rustdesk.portable/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definitio
 $exeFile               = Join-Path $toolsDir 'rustdesk-qs.exe'
 
 $url                   = 'https://github.com/rustdesk/rustdesk/releases/download/1.4.4/rustdesk-1.4.4-x86-sciter.exe'
-$checksum              = 'fa602089fe86db2021ac226eafa13fbcbd71767851010483345977f62db90f0f'
+$checksum              = '1a1b659e724e9bdfcce1532e76070a7502d1f35f1d74d60e3877a7a9dc5b4dd1'
 $ChecksumType          = 'sha256'
 $url64                 = 'https://github.com/rustdesk/rustdesk/releases/download/1.4.4/rustdesk-1.4.4-x86_64.exe'
-$checksum64            = '2ed35b0465fc3de72b0757e84daac278619edc30e651e6257073dceae5085332'
+$checksum64            = 'c612fd5a91b2d83dd9761f1979543ce05f6fa1941de3e00e40f6c7cdb3d4a6a0'
 $ChecksumType64        = 'sha256'
 
 $packageArgs = @{
