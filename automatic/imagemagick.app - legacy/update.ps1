be9d752b3a9e8c7f9e0792a324b7cfe8ec664730
Brian Dukes <bdukes@engagesoftware.com>
2026-01-12 09:52:29 -0600

    Make imagemagick patterns non-greedy

--- automatic/imagemagick.app - legacy/update.ps1
+++ automatic/imagemagick.app - legacy/update.ps1
@@ -18,8 +18,8 @@ function global:au_BeforeUpdate {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri 'https://legacy.imagemagick.org/script/download.php' -UseBasicParsing;
 
-    $url32 = $download_page | Select-String -Pattern 'http.+ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-x86-dll.exe';
-    $url64 = $download_page | Select-String -Pattern 'http.+ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-x64-dll.exe';
+    $url32 = $download_page | Select-String -Pattern 'http.+?ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-x86-dll.exe';
+    $url64 = $download_page | Select-String -Pattern 'http.+?ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-x64-dll.exe';
 
     $version = $url64.Matches[0].Groups[1].Value -replace '-', '.';
 
--- automatic/imagemagick/update.ps1
+++ automatic/imagemagick/update.ps1
@@ -11,8 +11,8 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri 'https://imagemagick.org/script/download.php' -UseBasicParsing;
 
-    $url32 = $download_page | Select-String -Pattern 'http.+ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-x86-dll.exe';
-    $url64 = $download_page | Select-String -Pattern 'http.+ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-HDRI-x64-dll.exe';
+    $url32 = $download_page | Select-String -Pattern 'http.+?ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-x86-dll.exe';
+    $url64 = $download_page | Select-String -Pattern 'http.+?ImageMagick-(\d+\.\d+\.\d+-\d+)-Q16-HDRI-x64-dll.exe';
 
     $version = $url64.Matches[0].Groups[1].Value -replace '-', '.';
 
