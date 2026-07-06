035ed3bcef4ca06dc214dfddb3a0b87081c5b250
Brian Dukes (via Azure DevOps) <bdukes@engagesoftware.com>
2026-07-06 10:02:43 +0000

    AU: 1 updated - elm-platform [skip ci] https://gist.github.com/bdukes/fd3d636b9b4617772f9b99ceb88838fa/ab6836772e931b8dde929fd50861bc97abd666b7

--- automatic/elm-platform/tools/chocolateyInstall.ps1
+++ automatic/elm-platform/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName   = 'elm-platform'
   fileType      = 'EXE'
-  url           = 'https://github.com/elm/compiler/releases/download/0.19.1/installer-for-windows.exe'
+  url           = 'https://github.com/elm/compiler/releases/download/0.19.2/installer-for-windows.exe'
   silentArgs    = '/S'
   validExitCodes= @(0)
   softwareName  = 'Elm Platform*'
-  checksum      = '02947D00090F556ABCF8F8E95997AC993825BD783ABF05786AFE8F60A002D496'
+  checksum      = '9de9ae1be7049391e1b353ecbe4d13f7527777b9c5e6ebb212b52b31b3fdc2be'
   checksumType  = 'sha256'
 }
 
